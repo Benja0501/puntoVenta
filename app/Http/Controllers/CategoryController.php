98e6f6c02fec53ca8c264610d36c0eeb2df640db
Benja0501 <G2333300121@unitru.edu.pe>
2025-05-22 14:44:53 -0500

    mi primer commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::get();
+        return view('admin.category.index', compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+
+    public function store(StoreCategoryRequest $request)
+    {
+        Category::create($request->all());
+        return redirect()->route('categories.index')->with('success', 'Category created successfully');
+    }
+
+    public function show(Category $category)
+    {
+        return view('admin.category.show', compact('category'));
+    }
+
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        //
+    }
+
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
